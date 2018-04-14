86715ed313ae621b0de4793245b8efaef36646ba
Balu <balumatta01@gmail.com>
2018-04-14 08:28:47 +0530

    modified run.py

--- run.py
+++ run.py
@@ -6,25 +6,45 @@ app = Flask("mini-amazon", static_url_path="");
 def health():
     return 'healthy \n'
 
-
-
+product_list = []
+indexPosition = 0
 @app.route('/', methods=['GET'])
 def index():
     return send_from_directory('staticHtmlFiles',"index.html")
 
 
 
-@app.route('/addProducts', methods=['POST'])
+@app.route('/addProducts', methods=['POST','GET'])
 def addProducts():
 
-    prod = {}
-    prod['product name'] = request.form['name'];
-    prod['product description'] = request.form['desc'];
-    prod['cost'] = request.form['cost'];
+    if request.method == 'POST':
+        prod = {}
+        prod['product name'] = request.form['name'];
+        prod['product description'] = request.form['desc'];
+        prod['cost'] = request.form['cost'];
+        product_list.append(prod)
+        return Response("Product with details " + str(prod) + "successfully added........")
 
-    print(prod)
+    elif request.method == 'GET':
+        for item in product_list:
+            if item['product name'] == request.args['name']:
+                return Response("Product details are " + str(item))
+            else:
+                return Response("Sorry!!! The product you searched is not found")
 
-    return Response('OK', 200)
+
+
+@app.route('/listProducts', methods=['GET'])
+def listProducts():
+    count = 0
+    listofItems = ""
+    if len(product_list) > 0:
+            for item in product_list:
+                count+=1
+                listofItems += str(count) + ". " + str(item) + "\n"
+                return Response(listofItems)
+    else:
+        return Response("Empty List! No data found.")
 
 
 if __name__ == '__main__':
