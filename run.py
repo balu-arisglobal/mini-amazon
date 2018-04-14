02415b4522f5cf1ac33673b0dbcc8e5ddd840732
Balu <balumatta01@gmail.com>
2018-04-14 17:15:59 +0530

    added modified files along with git ignore

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from flask import Flask, send_from_directory, request, Response
+from pymongo import MongoClient
+
+client = MongoClient('localhost',27017)
+db = client.miniamazonDB
+
 
 app = Flask("mini-amazon", static_url_path="");
 
-@app.route('/health', methods=['GET'])
-def health():
-    return 'healthy \n'
-
-product_list = []
 indexPosition = 0
 @app.route('/', methods=['GET'])
 def index():
@@ -14,7 +14,7 @@ def index():
 
 
 
-@app.route('/addProducts', methods=['POST','GET'])
+@app.route('/products', methods=['POST','GET'])
 def addProducts():
 
     if request.method == 'POST':
@@ -22,29 +22,33 @@ def addProducts():
         prod['product name'] = request.form['name'];
         prod['product description'] = request.form['desc'];
         prod['cost'] = request.form['cost'];
-        product_list.append(prod)
+        db.products.insert(prod)
         return Response("Product with details " + str(prod) + "successfully added........")
 
     elif request.method == 'GET':
-        for item in product_list:
-            if item['product name'] == request.args['name']:
-                return Response("Product details are " + str(item))
-            else:
-                return Response("Sorry!!! The product you searched is not found")
+        matching_items = db.products.find({'product name': {"$regex": request.args['name']}})
+
+        if matching_items.count() <=0:
+            return Response("Sorry!!! The product you searched is not found")
+        matches = []
+        for item in matching_items:
+            matches.append(item)
+        return Response("Product details are " + str(matches), mimetype="application/json")
+
 
 
 
 @app.route('/listProducts', methods=['GET'])
 def listProducts():
     count = 0
-    listofItems = ""
-    if len(product_list) > 0:
-            for item in product_list:
-                count+=1
-                listofItems += str(count) + ". " + str(item) + "\n"
-                return Response(listofItems)
+    allItems = db.products.find()
+    if allItems.count() > 0:
+        matches = []
+        for item in allItems:
+            matches.append(item)
+        return Response("Product details are " + str(matches) , mimetype="application/json" )
     else:
-        return Response("Empty List! No data found.")
+        return Response("Empty List! No data found.", mimetype="text/plain")
 
 
 if __name__ == '__main__':
